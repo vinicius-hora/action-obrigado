273a88375b14e7f217281675682dd9cf915bd16b
vinicius-hora <vinicius_hora@live.com>
2021-12-22 08:50:17 -0300

    2 commit

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run(){
-    console.log('Olá mundo!');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { contex = {} } = github;
+    const { pull_request } = contex.payload;
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: 'Obrigado pelo novo Pull Request!'
+      });
 }
+
 
 run();
