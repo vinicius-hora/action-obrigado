0b3aa99c2d43f3e11cd3ff7c8f0b8020b3f7b114
vinicius-hora <vinicius_hora@live.com>
2021-12-22 09:19:22 -0300

    teste3

--- dist/index.js
+++ dist/index.js
@@ -8449,13 +8449,13 @@ async function run(){
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const { contex = {} } = github;
-    const { pull_request } = contex.payload;
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
 
     await octokit.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
-        body: 'Obrigado pelo novo Pull Request!'
+        body: 'Obrigado pelo Pull Request!'
       });
 }
 
