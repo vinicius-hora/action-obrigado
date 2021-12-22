3c9e7ff0c29906240d3e7e5a50e943137645a1b8
vinicius-hora <vinicius_hora@live.com>
2021-12-22 11:19:57 -0300

    erro node:http 2.5

--- src/action.js
+++ src/action.js
@@ -18,7 +18,7 @@ async function run(){
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.rest.issues.createComment({
+    await octokit.issues.createComment({
       ...context.repo,
       issue_number: pull_request.number,
       body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`
